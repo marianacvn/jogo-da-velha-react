cc5aefcc47a274a4ff1f7143d4deaed33270ee4c
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-19 19:08:24 -0300

    Adicionei a logo da madder code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+
 import Sobre from './objects/Sobre/index'
+import Logo from './objects/Logo/index'
 
 function App() {
   return (
-    <Sobre />
+    <>
+      <Logo />
+      <Sobre />
+    </>
+    
   );
 }
  
---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,7 @@
+import Madder from '../../img/maddercode-logotipo-mi.png';
+
+const Logo = () => (
+    <img src={Madder} alt="Logo da Madder Code" />
+);
+
+export default Logo;
