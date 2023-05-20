58b12ee60a3a583ac5ab56d70edda93bb7494f1e
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-20 00:02:33 -0300

    Criação do Checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
-import Input from './objects/Input/index';
+import CheckBox from './objects/Checkbox';
 
 const App = () => (
     <main className="app">
       <Header />
       <Hashtag />
-      <Input id="show" value="show" content="Mostrar eventos"/>
+      <CheckBox id="show" value="show" type="checkbox" content="Mostrar eventos"/>
     </main>
  
   );
 
- 
 export default App;
--- src/objects/Input/index.jsx
+++ src/objects/Input/index.jsx
@@ -1,12 +1,7 @@
 import Label from "../Label";
 
-
-const Input = ({ id, value, content}) => (
-    <>
-    <input id={id} type="checkbox" value={value} />
-    <Label htmlFor={id} content= {content}/>
-    </>
-    
+const Input = ({ id = "", value = "", type = "text"}) => (
+        <input className="input" id={id} type={type} value={value} />
 );
 
 export default Input;
