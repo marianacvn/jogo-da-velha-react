b65c8e7a894f8394a51d0ac5f0eac38b75cf0be5
Mariana Carvalho <marianacvn1@gmail.com>
2023-05-19 23:25:57 -0300

    Criação do Label

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
+import Label from './objects/Label';
+import Input from './objects/Input/index';
 
-function App() {
-  return (
-    <>
-      <Header/>
+const App = () => (
+    <main className="app">
+      <Header />
       <Hashtag />
-      
-    </>
-    
+      <Input />
+      <Label content="Mostrar eventos"/>
+    </main>
+ 
   );
-}
+
  
 export default App;
---
+++ src/objects/Label/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const Label = ({ content }) => (
+    <label className="label">{content}</label>
+);
+
+export default Label;
